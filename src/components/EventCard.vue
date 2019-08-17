e9b24f3c08bbbb14d8f026bb2cc70e8e7a1e831f
Volodymyr Havryliuk <volodymyr.havryliuk@student.put.poznan.pl>
2019-08-17 22:57:35 +0300

    Completed course.

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,55 +1,45 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
-        <div class="event-card -shadow">
-            <span class="eyebrow">@{{event.time}} on {{event.date}}</span>
-            <h4 class="title">{{ event.title }}</h4>
-            <BaseIcon name="users">{{event.attendees.length}} attending</BaseIcon>
-        </div>
-    </router-link>
+    :to="{ name: 'event-show', params: { id: event.id } }"
+  >
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
+    </div>
+  </router-link>
 </template>
 
 <script>
-    import BaseIcon from "./BaseIcon";
-    export default {
-        components: {BaseIcon},
-        data() {
-            return {
-                event: {
-                    id: 1,
-                    title: "Beach Cleanup",
-                    date: "Tues Aug 19, 2018",
-                    time: "6:00",
-                    attendees: [
-                        {id: "abc123", name: "Adam Jahr"},
-                        {id: "def456", name: "Gregg Pollack"}
-                    ]
-                }
-            }
-        }
-    }
+import BaseIcon from "./BaseIcon"
+export default {
+  components: { BaseIcon },
+  props: {
+    event: Object
+  }
+}
 </script>
 <style scoped>
-    .event-card {
-        padding: 20px;
-        margin-bottom: 24px;
-        transition: all 0.2s linear;
-        cursor: pointer;
-    }
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
 
-    .event-card:hover {
-        transform: scale(1.01);
-        box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
-    }
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
 
-    .event-card > .title {
-        margin: 0;
-    }
+.event-card > .title {
+  margin: 0;
+}
 
-    .event-link {
-        color: black;
-        text-decoration: none;
-        font-weight: 100;
-    }
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
 </style>
