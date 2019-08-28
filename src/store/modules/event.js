d960fcfb85e94190176d5c369b63df3b1b292e95
volodymyr.havryliuk <volodymyr.havryliuk@demicon.de>
2019-08-29 00:42:21 +0300

    Completed Vue Error Handling and Base Input.

--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -57,25 +57,17 @@ export const actions = {
         dispatch("notification/add", notification, { root: true })
       })
   },
-  fetchEvent({ commit, getters, dispatch }, id) {
+  fetchEvent({ commit, getters }, id) {
     var event = getters.getEventById(id)
 
     if (event) {
       commit("SET_EVENT", event)
       return event
     } else {
-      return EventService.getEvent(id)
-        .then(response => {
-          this.event = response.data
-          commit("SET_EVENT", response.data)
-        })
-        .catch(error => {
-          const notification = {
-            type: "error",
-            message: "There was a problem fetching event: " + error.message
-          }
-          dispatch("notification/add", notification, { root: true })
-        })
+      return EventService.getEvent(id).then(response => {
+        this.event = response.data
+        commit("SET_EVENT", response.data)
+      })
     }
   }
 }
